75f4d83260ac4dcc4922c6729a0618852f35b899
Vamshi Suram <vamshi.suram25@gmail.com>
2023-05-10 22:50:26 +0530

    sql query and data return

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
 app = Flask(__name__)
 DB_USER = 'root'
@@ -15,8 +16,9 @@ class Employees(db.Model):
     EmployeeID = db.Column(db.Integer, primary_key=True)
     FirstName = db.Column(db.String(50))
 
-    def as_dict(self):
-        return {'EmployeeID': self.EmployeeID, 'FirstName': self.FirstName}
+    def __str__(self):
+        # return json.dumps(dict(self), ensure_ascii=False)
+        return f'EmployeeID: {self.EmployeeID}, FirstName: {self.FirstName}'
 
 
 @app.route("/")
@@ -24,10 +26,18 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route("/execute")
+@app.route("/execute", methods=('GET', 'POST'))
 def execute():
-    employees = Employees.query.all()
-    return jsonify([employee.as_dict() for employee in employees])
+    # employees = Employees.query.all()
+    # return jsonify([employee.as_dict() for employee in employees])
+    statement = '''
+      SELECT * FROM Employees;
+    '''
+    with db.engine.connect() as conn:
+        results = conn.execute(text(statement)).fetchall()
+        results = [tuple(row) for row in results]
+        # results = [tuple(row) for row in results]
+    return jsonify(dict(data=results))    # data contains dataframe!
 
 
 if __name__ == '__main__':
